9bd008db6cc9fd3265e9a9f1b6d7cdeeb88c1ac7
leonidamaltsev <daf.dof@yandex.ru>
2024-03-04 00:23:11 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/' };
+        int i = 0;
+        try {
+            while (!Thread.currentThread().isInterrupted() && i < process.length) {
+                System.out.print("\r load : " + process[i++]);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
