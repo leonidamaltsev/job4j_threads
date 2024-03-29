55d580de005ef6e1730c86672f17ad1f60d2204c
leonidamaltsev <daf.dof@yandex.ru>
2024-03-29 16:50:18 +0300

    4. Thread без общих ресурсов [#267919 #488679]

--- src/main/java/ru/job4j/ref/UserCache.java
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -5,22 +5,28 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Чтобы метод findAll() избавился от общих ресурсов, и работал с копиями объекта User,
+ * перебираем в цикле users.values(), создаем локальные копии объектов и возвращаем их в виде списка.
+ */
+
 public class UserCache {
     private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
     private final AtomicInteger id = new AtomicInteger();
 
     public void add(User user) {
-      /*users.put(id.incrementAndGet(), user);*/
         users.put(id.incrementAndGet(), User.of(user.getName()));
     }
 
     public User findById(int id) {
-      /*return users.get(id);*/
         return User.of(users.get(id).getName());
     }
 
     public List<User> findAll() {
         List<User> userList = new ArrayList<>(users.values());
+        for (User user : users.values()) {
+            userList.add(User.of(user.getName()));
+        }
         return userList;
     }
 }
