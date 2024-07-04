bff5314161d503eb0ed0375c918a252dd0efd7da
leonidamaltsev <daf.dof@yandex.ru>
2024-07-04 22:21:50 +0300

    1. Неблокирующий кеш [#4741 #503381]

--- src/main/java/ru/job4j/cache/Cache.java
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
 public class Cache {
@@ -15,7 +14,14 @@ public class Cache {
     }
 
     public boolean update(Base model) throws OptimisticException {
-        return memory.computeIfPresent(model.id(), compute(model)) != null;
+        return memory.computeIfPresent(model.id(), (key, value) -> {
+            Base stored = memory.get(model.id());
+            if (stored.version() != model.version()) {
+                throw new OptimisticException("Versions are not equal");
+            }
+            int newVersion = model.version() + 1;
+            return new Base(key, model.name(), newVersion);
+        }) != null;
     }
 
     public void delete(int id) {
@@ -26,14 +32,5 @@ public class Cache {
         return Stream.of(memory.get(id))
                 .filter(Objects::nonNull)
                 .findFirst();
-    }
-
-    private BiFunction<Integer, Base, Base> compute(Base model) throws OptimisticException {
-        Base stored = memory.get(model.id());
-        if (stored.version() != model.version()) {
-            throw new OptimisticException("Versions are not equal");
-        }
-        int newVersion = model.version() + 1;
-        return (key, value) -> new Base(key, model.name(), newVersion);
     }
 }
